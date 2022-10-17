27079b863d3a4f32fb137c056eb3baea24ea41b1
Siriphum Chiaravanont <Cchiaravanont@gmail.com>
2022-10-17 18:37:57 +0000

    completed javascript-operators-and-expressions exercise

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,46 @@
+var width = 20;
+var height = 20;
+var area = width * height;
+
+console.log('Value of area:', area);
+console.log('Type of area:', typeof area);
+
+var bill = 250;
+var payment = 300;
+var change = payment - bill;
+
+console.log('Value of change:', change);
+console.log('Type of change:', typeof change);
+
+var quizzes = 79;
+var midterm = 19;
+var final = 85;
+var grade = (quizzes + midterm + final) / 3;
+
+console.log('Value of grade:', grade);
+console.log('Type of grade:', typeof grade);
+
+var firstName = 'Chai';
+var lastName = 'Chiaravanont';
+var fullName = firstName + ' ' + lastName;
+
+console.log('Value of fullName:', fullName);
+console.log('Type of fullName:', typeof fullName);
+
+var pH = 6;
+var isAcidic = pH < 7;
+
+console.log('Value of isAcidic:', isAcidic);
+console.log('Type of isAcidic:', typeof isAcidic);
+
+var headCount = 299;
+var isSparta = headCount === 300;
+
+console.log('Value of isSparta:', isSparta);
+console.log('Type of isSparta:', typeof isSparta);
+
+var motto = fullName;
+motto += ' is the GOAT';
+
+console.log('Value of motto:', motto);
+console.log('Type of motto:', typeof motto);
