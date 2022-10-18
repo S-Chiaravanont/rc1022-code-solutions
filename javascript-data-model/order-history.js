edf25c9101c709393a523f16eeb6b70486eade84
Siriphum Chiaravanont <Cchiaravanont@gmail.com>
2022-10-18 18:54:41 +0000

    revise data structure with actual data instead of placeholder

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,39 +1,77 @@
 var orderHistory = [
   {
-    order_date: 'date',
-    orderConfirmation: 'string',
-    buyerInfo: 'string', // string of url to userAccout
-    totalCost: 0, // number
-    deliveryInfo: 'string',
+    order_date: 'August 4 2020',
+    orderConfirmation: '114-3941689-8772232',
+    buyerInfo: 'JS Masher',
+    totalCost: 34,
+    deliveryInfo: 'Aug 8, 2020',
     orderItems: [
       {
-        nameOfItem: 'string',
-        productDescription: 'string',
-        distributor: 'string',
-        productCategory: 'string', // electronic, clothes, beauty
-        price: 0, // number
-        itemImage: 'string url', // URL in string form
-        miscellaneousInfo: {
-          // for example books
-          isbn: 'string',
-          title: 'string',
-          author: 'string',
-          publish_date: 'string'
-        }
+        nameOfItem: 'JavaScript for impatient programmers',
+        productCategory: 'book',
+        price: 31.55,
+        itemImage: 'string url',
+        author: 'Rauschmayer, Dr. Axel',
+        returnWindow: 'Sep 7, 2020'
+      }
+    ]
+  },
+  {
+    order_date: 'July 19 2020',
+    orderConfirmation: '113-9984268-1280257',
+    buyerInfo: 'JS Masher',
+    totalCost: 44.53,
+    deliveryInfo: 'July 20, 2020',
+    orderItems: [
+      {
+        nameOfItem: 'The Timeless Way of Building',
+        productCategory: 'book',
+        price: 41.33,
+        itemImage: 'string url',
+        author: 'Alexander, Christopher',
+        returnWindow: 'Aug 19, 2020'
+      }
+    ]
+  },
+  {
+    order_date: 'July 4 2020',
+    orderConfirmation: '114-2875557-9059409',
+    buyerInfo: 'JS Masher',
+    totalCost: 17.22,
+    deliveryInfo: 'July 7, 2020',
+    orderItems: [
+      {
+        nameOfItem: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and vibration Features. No Driver and No Lag-Gamecube Adapter',
+        productCategory: 'electronic',
+        price: 15.98,
+        itemImage: 'string url',
+        author: null,
+        returnWindow: 'Aug 5, 2020'
+      }
+    ]
+  },
+  {
+    order_date: 'July 3 2020',
+    orderConfirmation: '113-2883177-2648248',
+    buyerInfo: 'JS Masher',
+    totalCost: 138.93,
+    deliveryInfo: 'July 7, 2020',
+    orderItems: [
+      {
+        nameOfItem: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        productCategory: 'electronic',
+        price: 94.95,
+        itemImage: 'string url',
+        author: null,
+        returnWindow: 'Aug 4, 2020'
       },
       {
-        nameOfItem: 'string',
-        productDescription: 'string',
-        distributor: 'string',
-        productCategory: 'string',
-        price: 0,
+        nameOfItem: 'The Art of Sql',
+        productCategory: 'book',
+        price: 33.99,
         itemImage: 'string url',
-        miscellaneousInfo: {
-          isbn: 'string',
-          title: 'string',
-          author: 'string',
-          publish_date: 'string'
-        }
+        author: 'Faroult, Stephane',
+        returnWindow: 'Aug 4, 2020'
       }
     ]
   }
