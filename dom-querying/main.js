7f739728521dd60657e2f69783564f4d1ac265de
Siriphum Chiaravanont <Cchiaravanont@gmail.com>
2022-10-24 17:08:25 +0000

    completed dom-querying exercise

---
+++ dom-querying/main.js
@@ -0,0 +1,13 @@
+console.log('hello, world');
+
+console.log(document.querySelector('h1'));
+console.log(console.dir(document.querySelector('h1')));
+
+console.log(document.querySelector('#explanation'));
+console.log(console.dir(document.querySelector('#explanation')));
+
+console.log(document.querySelector('.hint'));
+console.log(console.dir(document.querySelector('.hint')));
+
+console.log(document.querySelectorAll('p'));
+console.log(document.querySelectorAll('.example-link'));
