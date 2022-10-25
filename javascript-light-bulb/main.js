00a6bb7d67fac0857e985544740473931355b403
Siriphum Chiaravanont <Cchiaravanont@gmail.com>
2022-10-25 18:51:11 +0000

    revise javascript file to remove the use of DOM data on conditional statement and create new javascript variable in place

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,14 +1,16 @@
 var $circleButton = document.querySelector('.circle');
 $circleButton.addEventListener('click', switchColor);
 var $bodyBackground = document.querySelector('body');
+var isOn = true; // because initial state of light bulb is on
 
 function switchColor(event) {
-  var currentColor = $circleButton.className.split(' ')[1];
-  if (currentColor === 'bg-yellow') {
+  if (isOn) {
     $bodyBackground.className = 'bg-black';
     $circleButton.className = 'circle bg-darkgray';
+    isOn = false;
   } else {
     $bodyBackground.className = 'bg-white';
     $circleButton.className = 'circle bg-yellow';
+    isOn = true;
   }
 }
