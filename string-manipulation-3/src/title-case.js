f8603dfe113ae4a56975cb5328dc8e4c42357790
Siriphum Chiaravanont <Cchiaravanont@gmail.com>
2022-10-26 20:51:49 +0000

    in progress title-case exercise, added pseudo codes

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,4 +1,24 @@
 /* exported titleCase */
 
 /*
+- create an array of minor words
+- create variable for correct JavaScript capitalized
+- create variable for lower case JavaScript for condition
+- create variable for correct API capitalized
+- create variable for lower case API for condition
+- need to find out if individual word ends with colon so the next adjacent word will always get capitalized
+------------------
+- assuming input title letters are all lower case
+- Split input title with space to into a new array
 */
+
+// var minorWords = ['and', 'or', 'nor', 'but', 'a', 'an', 'the', 'as', 'at', 'by', 'for', 'in', 'of', 'on', 'per', 'to'];
+// var javaScript = 'JavaScript';
+// var lowerCaseJavaScript = 'javascript';
+// var acronymAPI = 'API';
+// var lowerCaseAPI = 'api';
+
+// function titleCase(title) {
+//   var titleArray = title.split(' ');
+
+// }
