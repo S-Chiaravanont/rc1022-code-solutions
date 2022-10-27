86ce32afc8a79ec4b2e9e7ff56c9b9ae69d34efd
Siriphum Chiaravanont <Cchiaravanont@gmail.com>
2022-10-27 23:27:01 +0000

    remove all commented codes

--- javascript-view-swapping/main.js
+++ javascript-view-swapping/main.js
@@ -5,21 +5,14 @@ var $views = document.querySelectorAll('.view');
 $tabContainer.addEventListener('click', handleClick);
 
 function handleClick(event) {
-  // console.log('event.target.className:', event.target.className);
-  // console.log(event.target.matches('.tab'));
   if (!event.target.matches('.tab')) {
-    // console.log('tab is clicked');
     return;
   }
-  // console.log(event.target);
-  // console.log($views);
   for (var i = 0; i < $tabs.length; i++) {
     if (event.target.textContent === $tabs[i].textContent) {
       $tabs[i].className = 'tab active';
-      // $views[i].setAttribute('class', 'view');
     } else {
       $tabs[i].className = 'tab';
-      // $views[i].setAttribute('class', 'view hidden');
     }
   }
   var dataView = event.target.getAttribute('data-view');
