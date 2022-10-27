8d507b6f67cc2caf52c0e0aaf42c75b4a0d2333e
Siriphum Chiaravanont <Cchiaravanont@gmail.com>
2022-10-27 23:25:15 +0000

    in progress, finished zip and unique js

--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -1,1 +1,25 @@
 /* exported unique */
+
+/*
+- check if input array is empty -> if yes return empty array []
+- create new empty array uniqueArray
+- push first item of the input array into uniqueArray because it's always unique
+- use for loop to iterate through each item of the array starting at index 1
+- check if array[i] is included in the uniqueArray
+- if not, push item onto uniqueArray
+- return uniqueArray
+*/
+
+function unique(array) {
+  if (array.length === 0) {
+    return [];
+  }
+  var uniqueArray = [];
+  uniqueArray.push(array[0]);
+  for (var i = 1; i < array.length; i++) {
+    if (!Array.prototype.includes.call(uniqueArray, array[i])) {
+      uniqueArray.push(array[i]);
+    }
+  }
+  return uniqueArray;
+}
