170cb6c2e1449bc7cd83312ddbbbd31a11c71623
Siriphum Chiaravanont <Cchiaravanont@gmail.com>
2022-11-08 19:16:33 +0000

    completed javascript-bank exercise

---
+++ javascript-bank/bank.js
@@ -0,0 +1,37 @@
+/* exported Bank */
+
+function Bank() {
+  this.nextAccountNumber = 1;
+  this.accounts = [];
+}
+
+Bank.prototype.openAccount = function (holder, balance) {
+  if (balance <= 0 || !Number.isInteger(balance)) {
+    return null;
+  }
+  var newAccount = new Account(this.nextAccountNumber, holder);
+  newAccount.deposit(balance);
+  this.accounts.push(newAccount);
+  this.nextAccountNumber++;
+  return newAccount.number;
+};
+
+Bank.prototype.getAccount = function (number) {
+  for (var i = 0; i < this.accounts.length; i++) {
+    if (parseInt(number) === parseInt(this.accounts[i].number)) {
+      return this.accounts[i];
+    }
+  }
+  return null;
+};
+
+Bank.prototype.getTotalAssets = function () {
+  if (this.accounts.length === 0) {
+    return 0;
+  }
+  var totalAssets = 0;
+  for (var i = 0; i < this.accounts.length; i++) {
+    totalAssets += this.accounts[i].getBalance();
+  }
+  return totalAssets;
+};
