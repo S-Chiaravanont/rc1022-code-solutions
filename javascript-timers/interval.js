c363589fc86e640ae8c879bee5e9f4a1d7ea7e2a
Siriphum Chiaravanont <Cchiaravanont@gmail.com>
2022-11-09 18:15:09 +0000

    revise interval.js to remove switch statement and change to different approach with backward counter

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -1,26 +1,14 @@
 
 var $countDown = document.querySelector('.countdown-display');
-var counter = 0;
+var counter = 4;
 function countDown() {
-  switch (counter) {
-    case 0 :
-      $countDown.textContent = '3';
-      counter++;
-      break;
-    case 1 :
-      $countDown.textContent = '2';
-      counter++;
-      break;
-    case 2 :
-      $countDown.textContent = '1';
-      counter++;
-      break;
-    case 3 :
-      $countDown.textContent = '~Earth Beeeelooowww Us~';
-      counter++;
-      clearInterval(intervalID);
-      break;
+  counter--;
+  if (counter === 0) {
+    $countDown.textContent = '~Earth Beeeelooowww Us~';
+    clearInterval(intervalID);
+    return;
   }
+  $countDown.textContent = String(counter);
 }
 
 var intervalID = setInterval(countDown, 1000);
