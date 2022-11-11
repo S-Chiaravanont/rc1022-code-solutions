ff666bd0873f35cb060414f589be08ee8f1d3a36
Siriphum Chiaravanont <Cchiaravanont@gmail.com>
2022-11-11 17:50:12 +0000

    completed javascript-ajax exercise

---
+++ javascript-ajax/users.js
@@ -0,0 +1,15 @@
+var $userList = document.querySelector('#user-list');
+
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+xhr.responseType = 'json';
+xhr.addEventListener('load', function () {
+  console.log(xhr.status);
+  console.log(xhr.response);
+  for (var i = 0; i < xhr.response.length; i++) {
+    var $newUser = document.createElement('li');
+    $newUser.textContent = xhr.response[i].name;
+    $userList.appendChild($newUser);
+  }
+});
+xhr.send();
