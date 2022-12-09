31131a7019471adf7a44b33c19afa2ec168d5eb5
Siriphum Chiaravanont <Cchiaravanont@gmail.com>
2022-12-09 22:09:39 +0000

    revise function to use .toFixed to evaluate prices

--- array-map/main.js
+++ array-map/main.js
@@ -11,7 +11,7 @@ const languages = [
 const doubled = numbers.map(x => x * 2);
 console.log(doubled);
 
-const prices = numbers.map(x => `$${x}.00`);
+const prices = numbers.map(x => `$${x.toFixed(2)}`);
 console.log(prices);
 
 const upperCased = languages.map(lang => lang.toUpperCase());
