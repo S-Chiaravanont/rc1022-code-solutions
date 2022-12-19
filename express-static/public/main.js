8b02e9531b01b3dfcd1940e990e100ec9fbe4a0c
Siriphum Chiaravanont <Cchiaravanont@gmail.com>
2022-12-19 18:28:25 +0000

    revise main.js, add index.js file to run the server from, and use dirPublic as argument for express static method

--- express-static/public/main.js
+++ express-static/public/main.js
@@ -1,17 +1,2 @@
-const express = require('express');
-const path = require('path');
-
-const app = express();
-
-const dirPublic = path.join(__dirname, 'public');
 // eslint-disable-next-line no-console
-console.log(dirPublic);
-
-const staticPublic = express.static('public');
-
-app.use(staticPublic);
-
-app.listen(3000, () => {
-  // eslint-disable-next-line no-console
-  console.log('listening on port 3000');
-});
+console.log('Static Middleware mounted successfully');
