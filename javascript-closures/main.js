6667c858fb695decdc69705879947a5f2936f5ae
Siriphum Chiaravanont <Cchiaravanont@gmail.com>
2023-01-12 00:35:00 +0000

    in progress

---
+++ javascript-closures/main.js
@@ -0,0 +1,14 @@
+// function graduate(credential) {
+//   let fullName = null;
+//   function changeName(val) {
+//     fullName = val;
+//     return fullName;
+//   }
+//   return {
+//     fullName(name) {
+//       changeName(name);
+//     }
+//   };
+// }
+
+// medicalSchool('name') = 'name, M.D.'
